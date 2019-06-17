0aa3eb8c66ab67e0de8c638f517823cd8a6b5cf9
이재호/연구원/On-Device Learning Task(jaeho56.lee@lge.com) <jaeho56.lee@lge.com>
2019-06-17 15:32:05 +0900

    [chapter05] decorator

---
+++ chapter05/README.md
@@ -0,0 +1,3 @@
+# Chapter 05
+
+## Code Enhancement using Decorators
---
+++ chapter05/jaeho/decorators.py
@@ -0,0 +1,35 @@
+import functools
+import time
+
+def do_twice(func):
+    """Run the decorated function twice"""
+    @functools.warps(func)
+    def wrapper_do_twice(*args, **kwrags):
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return wrapper_do_twice
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        value - func(*args, **kwargs)
+        end_time  = time.perf_counter()
+        run_time = end_time - start_time
+        print('Finished {} in {} secs'.format(func.__name__, run_time))
+        return value
+    return wrapper_timer
+
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kawrgs):
+        args_repr = [repr(a) for a in args]
+        kwrags_repr = ['{}={}'.format(k, v) for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwrags_repr)
+        print('Calling {}({})'.format(func.__name__, signature))
+        value = func(*args, **kwargs)
+        print('{} returned {}'.format(func.__name__, value))
+        return value
+    return wrapper_debug
